62edf9e1ebe19718dece7dab19248abfba1ed37a
victorbetoni <victorbetoni@protonmail.com>
2023-12-26 17:16:38 -0300

    fixing img path

--- src/components/crafter/Crafter.vue
+++ src/components/crafter/Crafter.vue
@@ -4,7 +4,7 @@
     <div class="flex flex-row justify-between mb-8">
       <p class="text-3xl text-mc-light-purple">GsWCrafter</p>
       <a class="flex p-2 rounded-md cursor-pointer py-3 gap-x-2" href="https://discord.gg/Nt2rPRHxyX" target="_blank">
-        <img src="/builder/discord.png" class="w-10 object-contain"/>
+        <img src="/discord.png" class="w-10 object-contain"/>
         <p class="my-auto text-xs text-mc-blue">Join our discord</p>
       </a>
     </div>
