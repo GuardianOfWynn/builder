38fa1b7c4bdc49e42ab4c250af903491389a3a4c
victorbetoni <victorbetoni@protonmail.com>
2024-01-03 23:31:38 -0300

    feat: implementing ability tree build

--- src/components/builder/AbilityTree.vue
+++ src/components/builder/AbilityTree.vue
@@ -10,14 +10,18 @@
         <div class="p-2">
             <div v-for="i in getDepth()" class="flex">
             <div v-for="j in 9">
-                <div class="w-10 h-10" @click="">
-                    <img class="w-10 h-10 scale-125" v-if="hasNode(j, i)" @click="toggleNode(getNodeFor(j,i)!)"
+                <div class="w-10 h-10">
+                    <div v-if="hasNode(j, i)" @click="toggleNode(getNodeFor(j, i)!)" class="cursor-pointer">
+                        <img class="w-10 h-10 object-fill scale-125 pixelated" v-if="selectedNodes.some(a => a.coordinates.x == j && a.coordinates.y == i)"
+                        :src="'/builder/sprites/abilitytree/' + getNodeFor(j, i)!.sprite.activated" />
+                        <img class="w-10 h-10 object-fill scale-125" v-else
                         :src="'/builder/sprites/abilitytree/' + getNodeFor(j, i)!.sprite.normal" />
+                    </div>
                     <div v-else-if="hasConnector(j, i)">
                         <img class="w-10 h-10 object-fill pixelated" v-if="activeConnectors.some(a => a.x == j && a.y == i)"
-                        :src="'/builder/sprites/abilitytree/' + getConnectorFor(j, i)!.sprite.activated" />
+                        :src="'/builder/sprites/abilitytree/' + getConnectorFor(j, i)!.type + '_1.png'" />
                         <img class="w-10 h-10 object-fill" v-else
-                        :src="'/builder/sprites/abilitytree/' + getConnectorFor(j, i)!.sprite.normal" />
+                        :src="'/builder/sprites/abilitytree/' + getConnectorFor(j, i)!.type + '.svg'" />
                     </div>
                     
                 </div>
@@ -32,7 +36,7 @@
 <script lang="ts">
 
 import { Ref, ref, watchEffect } from 'vue';
-import { AbilityNode, AbilityNodeConnector, findPath } from '../../model/abilitytree'
+import { AbilityNode, AbilityNodeConnector, makeTree } from '../../model/abilitytree'
 
 export default {
     name: 'AbilityTree',
@@ -42,13 +46,9 @@ export default {
     },
     setup(props) {
         const abilityTree: Ref<AbilityNode[] | undefined> = ref(undefined);
-        let str = props.tree![36];
-        let CHEAPER = props.tree![34];
 
-        const selectedNodes: AbilityNode[] = [];
+        const selectedNodes: Ref<AbilityNode[]> = ref([]);
         const activeConnectors: Ref<AbilityNodeConnector[]> = ref([]);
-
-       activeConnectors.value = findPath(str, CHEAPER, props.connectors!, props.tree!);
 
         const getNodeFor = (x: number, y: number): AbilityNode | undefined => props.tree?.filter(a => a.coordinates.x === x && a.coordinates.y === y)[0];
         const hasNode = (x: number, y: number): boolean => getNodeFor(x,y) !== undefined;
@@ -59,19 +59,25 @@ export default {
         const getDepth = (): number => Math.max(...props.tree!.map(x => x.coordinates.y))
 
         const toggleNode = (abilityNode: AbilityNode) => {
-            if(selectedNodes.some(x => x.id === abilityNode.id)) {
-                selectedNodes.splice(selectedNodes.findIndex(x => x.id === abilityNode.id));
+            if(selectedNodes.value.some(x => x.id === abilityNode.id)) {
+                selectedNodes.value.splice(selectedNodes.value.findIndex(x => x.id === abilityNode.id));
             } else {
-                selectedNodes.push(abilityNode);
+                selectedNodes.value.push(abilityNode);
             }
+            assemble();
         } 
+
+        const assemble = () => {
+            let tree = makeTree(selectedNodes.value, props.tree!, props.connectors!);
+            activeConnectors.value = tree.connectors;
+        }
 
         watchEffect(() => {
             abilityTree.value=props.tree;
         })
 
         return {
-            abilityTree, getDepth, getNodeFor, toggleNode,getConnectorFor,hasNode,hasConnector,activeConnectors
+            abilityTree, getDepth, getNodeFor, toggleNode,getConnectorFor,hasNode,hasConnector,activeConnectors,selectedNodes
         }
     },
     components: {}
--- src/model/abilitytree.ts
+++ src/model/abilitytree.ts
@@ -1,5 +1,7 @@
 import { Pair } from "../scripts/util";
 
+export const BASE_ABILITY_POINTS = 45;
+
 export const WARRIOR_ABILITY_TREE: AbilityNode[] = await (await fetch("/builder/trees/warrior.json")).json() ;
 export const WARRIOR_CONNECTORS: AbilityNodeConnector[] = await (await fetch("/builder/trees/warrior_connectors.json")).json() ;
 
@@ -8,13 +10,108 @@ export const ARCHER_ABILITY_TREE: AbilityNode[] = await (await fetch("/builder/t
 export const SHAMAN_ABILITY_TREE: AbilityNode[] = await (await fetch("/builder/trees/shaman.json")).json();
 export const ASSASSIN_ABILITY_TREE: AbilityNode[] = await (await fetch("/builder/trees/assassin.json")).json();
 
+export enum Archetype {
+    ACOLYTE = "bloodmagik",
+    SUMMONER = "summoner",
+    RITUALIST = "ritualist",
+    RIFTWALKER = "blitz",
+    LIGHT_BENDER = "priest",
+    ARCANIST = "arcane",
+    BATTLE_MONK = "monk",
+    FALLEN = "berserker",
+    PALADIN = "tank",
+    TRAPPER = "trapper",
+    SHARPSHOOTER = "sniper",
+    BOLTSLINGER = "boltslinger",
+    SHADESTEPPER = "shadestepper",
+    TRICKSTER = "trickster",
+    ACROBAT = "acrobat"
+}
+
+export enum ConnectorType {
+    DOWN_LEFT = "connector_down_left",
+    DOWN_RIGHT = "connector_right_down",
+    RIGHT_DOWN_LEFT = "connector_right_down_left",
+    RIGHT_LEFT = "connector_right_left",
+    UP_DOWN = "connector_up_down",
+    UP_RIGHT_DOWN = "connector_up_right_down",
+    CROSS = "connector_up_right_down_left"
+
+}
+
+export enum Direction {
+    UP = "up",
+    DOWN = "down",
+    LEFT = "left",
+    RIGHT = "right"
+}
+
+export const CONNECTOR_TYPE_DIRECTIONS = new Map<ConnectorType, Direction[]>([
+    [ConnectorType.CROSS, [Direction.DOWN, Direction.LEFT, Direction.UP, Direction.RIGHT]],
+    [ConnectorType.DOWN_LEFT, [Direction.DOWN, Direction.LEFT]],
+    [ConnectorType.DOWN_RIGHT, [Direction.DOWN, Direction.RIGHT]],
+    [ConnectorType.RIGHT_DOWN_LEFT, [Direction.RIGHT, Direction.DOWN, Direction.LEFT]],
+    [ConnectorType.RIGHT_LEFT, [Direction.RIGHT, Direction.LEFT]],
+    [ConnectorType.UP_DOWN, [Direction.UP, Direction.DOWN]],
+    [ConnectorType.UP_RIGHT_DOWN, [Direction.RIGHT, Direction.DOWN, Direction.UP]]
+]);
+
+export function isDirectionAllowed(connector: ConnectorType, direction: Direction): boolean {
+    return CONNECTOR_TYPE_DIRECTIONS.get(connector)!.includes(direction);
+}
+
+export class AbilityTree {
+    baseTree: AbilityNode[];
+    nodes: AbilityNode[];
+    connectors: AbilityNodeConnector[];
+    availableNodes: AbilityNode[];
+
+    constructor(baseTree: AbilityNode[], nodes: AbilityNode[], connectors: AbilityNodeConnector[], availableNodes: AbilityNode[]) {
+        this.baseTree = baseTree;
+        this.nodes = nodes;
+        this.connectors = connectors;
+        this.availableNodes = availableNodes;
+    }
+    
+    getNodesOfArchetype(arch: Archetype): AbilityNode[] {
+        return this.nodes.filter(x => x.archetype !== "" && x.archetype === arch);
+    }
+
+    getAvailableAbilityPoints(): number {
+        return BASE_ABILITY_POINTS - this.nodes.reduce((ac, v) => ac + v.requirements.abilityPoints, 0);
+    }
+
+    getLockedAbilities(): AbilityNode[] {
+        let locked: AbilityNode[] = [];
+        this.nodes.forEach(ab => ab.locks.forEach(lo => {
+            if(!locked.some(x => x.id === lo)) {
+                locked.push(getAbilityNode(lo, this.baseTree))
+            }
+        }))
+        return locked;
+    }
+
+    isNodeAvailable(node: AbilityNode): boolean {
+        if(node.requirements.archetype !== undefined && this.getNodesOfArchetype(node.requirements.archetype.name as Archetype).length < node.requirements.archetype.amount) {
+            return false;
+        }
+        if(node.requirements.abilityPoints > this.getAvailableAbilityPoints()) {
+            return false;
+        }
+        if(node.requirements.node !== undefined && !this.nodes.some(x => x.id === node.requirements.node)) {
+            return false;
+        }
+        if(this.getLockedAbilities().some(x => x.id === node.id)) {
+            return false;
+        }
+        return true;
+    }
+}
+
 export class AbilityNodeConnector {
     x: number;
     y: number;
-    sprite: {
-        normal: string;
-        activated: string;
-    }
+    type: string;
 }
 
 export class AbilityNode {
@@ -49,9 +146,9 @@ export function findPath(root: AbilityNode, target: AbilityNode, connectors: Abi
     let downConnector = getConnectorAt(root.coordinates.x, root.coordinates.y+1, connectors);
     let leftConnector = getConnectorAt(root.coordinates.x-1, root.coordinates.y, connectors);
     let rightConnector = getConnectorAt(root.coordinates.x+1, root.coordinates.y, connectors);
+    console.log(leftConnector);
 
     if(upConnector !== undefined) {
-        console.log("up");
         let resultUp = expand(upConnector!, target, connectors, tree, []);
         if(resultUp.first) {
             return resultUp.second
@@ -59,7 +156,6 @@ export function findPath(root: AbilityNode, target: AbilityNode, connectors: Abi
     }
 
     if(downConnector !== undefined) {
-        console.log("down");
         let resultDown = expand(downConnector!, target, connectors, tree, []);
         if(resultDown.first) {
             return resultDown.second
@@ -67,7 +163,6 @@ export function findPath(root: AbilityNode, target: AbilityNode, connectors: Abi
     }
 
     if(leftConnector !== undefined) {
-        console.log("left");
         let resultLeft = expand(leftConnector!, target, connectors, tree, []);
         if(resultLeft.first) {
             return resultLeft.second
@@ -75,7 +170,6 @@ export function findPath(root: AbilityNode, target: AbilityNode, connectors: Abi
     }
 
     if(rightConnector !== undefined) {
-        console.log("right");
         let resultRight = expand(rightConnector!, target, connectors, tree, []);
         if(resultRight.first) {
             return resultRight.second
@@ -90,6 +184,7 @@ function expand(from: AbilityNodeConnector, target: AbilityNode, connectors: Abi
         return {first: false, second: visited}
     }
     visited.push(from);
+    console.log(visited);
     let upNode = getNodeAt(from.x, from.y-1, tree);
     let downNode = getNodeAt(from.x, from.y+1, tree);
     let leftNode = getNodeAt(from.x-1, from.y, tree);
@@ -107,29 +202,29 @@ function expand(from: AbilityNodeConnector, target: AbilityNode, connectors: Abi
     let leftConnector = getConnectorAt(from.x-1, from.y, connectors);
     let rightConnector = getConnectorAt(from.x+1, from.y, connectors);
 
-    if(!wasVisited(upConnector, visited)) {
+    if(upConnector !== undefined && isDirectionAllowed(upConnector.type as ConnectorType, Direction.UP) && !wasVisited(upConnector, visited)) {
         let result = expand(upConnector!, target, connectors, tree, visited);
         if(result.first) {
             return result;
         }
     }
 
-    if(!wasVisited(downConnector, visited)) {
+    if(downConnector !== undefined && isDirectionAllowed(downConnector.type as ConnectorType, Direction.DOWN) && !wasVisited(downConnector, visited)) {
         let result = expand(downConnector!, target, connectors, tree, visited);
         if(result.first) {
             return result;
         }
     }
 
-    if(!wasVisited(leftConnector, visited)) {
-        let result = expand(leftConnector!, target, connectors, tree, visited);
+    if(rightConnector !== undefined && isDirectionAllowed(rightConnector.type as ConnectorType, Direction.RIGHT) && !wasVisited(rightConnector, visited)){
+        let result = expand(rightConnector!, target, connectors, tree, visited);
         if(result.first) {
             return result;
         }
     }
 
-    if(!wasVisited(rightConnector, visited)){
-        let result = expand(rightConnector!, target, connectors, tree, visited);
+    if(leftConnector !== undefined && isDirectionAllowed(leftConnector.type as ConnectorType, Direction.LEFT) && !wasVisited(leftConnector, visited)) {
+        let result = expand(leftConnector!, target, connectors, tree, visited);
         if(result.first) {
             return result;
         }
@@ -138,8 +233,27 @@ function expand(from: AbilityNodeConnector, target: AbilityNode, connectors: Abi
     return { first: false, second: visited } 
 }
 
+export function makeTree(selectedNodes: AbilityNode[], tree: AbilityNode[], connectors: AbilityNodeConnector[]): AbilityTree {
+    let paths: AbilityNodeConnector[] = [];
+    selectedNodes.forEach(node => {
+        if(node.links === null) {
+            return;
+        }
+        node.links.map(x => getAbilityNode(x, tree)).forEach(target => {
+            if(selectedNodes.includes(target)) {
+                paths.push(...findPath(node, target, connectors, tree));
+            }
+        })
+    });
+    return new AbilityTree(tree, selectedNodes, paths, []);
+}
+
+export function getAbilityNode(id: string, tree: AbilityNode[]): AbilityNode {
+    return tree.filter(x => x.id === id)[0];
+}
+
 function wasVisited(connector: AbilityNodeConnector | undefined, visited: AbilityNodeConnector[]): boolean {
-    return visited.some(a => connector !== undefined && a.x === connector!.x && a.y === connector!.y);
+    return visited.some(a => a.x === connector!.x && a.y === connector!.y);
 }
 
 export function getNodeAt(x: number, y: number, tree: AbilityNode[]): AbilityNode | undefined {
