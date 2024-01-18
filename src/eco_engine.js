fce527e5e9a35f68f61bbf2cdbe4636ba940345a
victorbetoni <victorbetoni@protonmail.com>
2024-01-18 11:26:15 -0300

    wasm just dont work for our purpose

--- src/eco_engine.js
+++
@@ -1,6 +0,0 @@
-import { SKY_CLAIM_PRESET, TERRITORIES } from "./model/ecoengine/ecoengine";
-import "../wasm_exec"
-
-onmessage = function(wasmInstance) {
-    wasmInstance.exports.startEngine(territories, preset)
-}
--- src/model/ecoengine/ecoengine.ts
+++ src/model/ecoengine/ecoengine.ts
@@ -1,2 +1,3 @@
 export const TERRITORIES: any = await (await fetch("/builder/territories.json")).json() ;
 export const SKY_CLAIM_PRESET: any = await (await fetch("/builder/presets/sky.json")).json() ;
+
