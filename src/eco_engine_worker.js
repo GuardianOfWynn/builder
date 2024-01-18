b7d222e0d02b930bcaf2f7b527589ca56bb99dfa
victorbetoni <victorbetoni@protonmail.com>
2024-01-18 10:23:34 -0300

    removing static imports

--- src/eco_engine_worker.js
+++ src/eco_engine_worker.js
@@ -1,21 +1,21 @@
-import { SKY_CLAIM_PRESET, TERRITORIES } from "./model/ecoengine/ecoengine";
 import "../wasm_exec"
 
 let wasm = undefined;
 
-const go = new Go();
-
-function StartEcoEngine() {
-	const go = new Go();
-	WebAssembly.instantiateStreaming(fetch("/builder/eco-engine.wasm"), go.importObject).then((res) => {
-		let territories = JSON.stringify(TERRITORIES)
-		let preset = JSON.stringify(SKY_CLAIM_PRESET)
-		wasm = res.instance
-		go.run(wasm)
-		wasm.exports.startEcoEngineTinyGo(territories, preset)
-	});
-}
-
-onmessage = function(e) {
+self.onmessage = function(e) {
+	console.log("aaaa")
     StartEcoEngine()
 };
+
+async function StartEcoEngine() {
+	const go = new Go();
+	WebAssembly.instantiateStreaming(fetch("/builder/eco-engine.wasm"), go.importObject).then((res) => {
+		fetch('/builder/territories.json').then((response) => response.json()).then(territories => {
+			fetch('/builder/presets/sky.json').then((response) => response.json()).then(preset => {
+				wasm = res.instance
+				go.run(wasm)
+				wasm.exports.startEngineTinyGo(JSON.stringify(territories), JSON.stringify(preset))
+			})
+		})
+	});
+}
